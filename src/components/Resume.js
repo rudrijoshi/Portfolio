0a49877b2104acefd37320c36571eb96f11e86d7
Mona Lisa <130613163+rudrijoshi@users.noreply.github.com>
2023-08-01 19:23:16 -0400

    made changes to resume.js file

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,7 @@
 import '../style/style.css';
 
+import { skills } from '../data.js';
+import React from 'react';
 
 export default function Resume() {
     const onBtnClick = () => {
@@ -19,9 +21,17 @@ export default function Resume() {
 
     }
     return (
-        <div className="resumeEdit p-2 text-dark">
-            <br />
-            <button className="resume btn btn-primary btn-lg" type="button" onClick={onBtnClick}>Download my Resume</button>
+        <div className="Skill p-2 text-white text-center">
+            <h1 className="pb-40">My Skiils </h1>
+            <div className="grid grid-cols-4 gap-5 place-items-center pb-44">
+                {skills.map((skill) => (<div key={skill.id} className="flex flex-row items-center p-1 border-solid border-4 rounded-md text-lg bg-cyan-800">
+                    <span key={skill.id} className="p-4">
+                        {skill.title}
+                    </span>
+                    <img src={skill.image} key={`${skill.id}-image`} alt='logo' className="w-18 h-14" />
+                </div>))}
+            </div>
+            <button className=" btn btn-primary btn-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" type="button" onClick={onBtnClick}>Download my Resume</button>
         </div>
     )
 };
---
+++ src/data.js
@@ -0,0 +1,89 @@
+import image1 from '../src/skills/html.png';
+import image2 from '../src/skills/css.png';
+import image3 from '../src/skills/javascript.png';
+import image4 from '../src/skills/node.png';
+import image5 from '../src/skills/nosql.png';
+import image6 from '../src/skills/mvc.png';
+import image7 from '../src/skills/graphql.png';
+import image8 from '../src/skills/sql.png';
+import image9 from '../src/skills/agile.png';
+import image10 from '../src/skills/restful.png';
+import image11 from '../src/skills/mongodb.png';
+import image12 from '../src/skills/pwa.png';
+import image13 from '../src/skills/github.png';
+import image14 from '../src/skills/react.png';
+
+
+
+export const skills = [
+    {
+        id: 1,
+        title: "HTML",
+        image: image1
+    },
+    {
+        id: 2,
+        title: "CSS",
+        image: image2
+    },
+    {
+        id: 3,
+        title: "JavaScript",
+        image: image3
+    },
+    {
+        id: 4,
+        title: "Node",
+        image: image4
+    },
+    {
+        id: 5,
+        title: "NoSQL",
+        image: image5
+    },
+    {
+        id: 6,
+        title: "MVC",
+        image: image6
+    },
+    {
+        id: 7,
+        title: "GraphQL",
+        image: image7
+    },
+    {
+        id: 8,
+        title: "SQL",
+        image: image8
+    },
+    {
+        id: 9,
+        title: "Agile",
+        image: image9
+    },
+    {
+        id: 10,
+        title: "Restful API",
+        image: image10
+    },
+    {
+        id: 11,
+        title: "MongoDB",
+        image: image11
+    },
+    {
+        id: 12,
+        title: "PWA",
+        image: image12
+    },
+    {
+        id: 13,
+        title: "GitHub",
+        image: image13
+    },
+    {
+        id: 14,
+        title: "React",
+        image: image14
+    }
+]
